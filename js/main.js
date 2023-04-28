2af6249f538f64ef0d9e3b2e8d2a904c1a403b67
Eduardo Paulo Tomilhero <Eduardo_tomi@hotmail.com>
2023-04-27 22:43:20 -0300

    finalização
    adicionar, atualizar e excluir

--- js/main.js
+++ js/main.js
@@ -1,23 +1,84 @@
 const form = document.getElementById("novoItem");
-const lista = document.getElementById("lista")
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
-form.addEventListener("submit", (evento)=>{
-    evento.preventDefault()
+itens.forEach((elemento) => {
+  criaElemento(elemento);
+});
 
-    criarElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
-})
+form.addEventListener("submit", (evento) => {
+  evento.preventDefault();
 
-function criarElemento(nome, quantidade) {
+  const nome = evento.target.elements["nome"];
+  const quantidade = evento.target.elements["quantidade"];
 
-    const novoItem = document.createElement('li')
-    novoItem.classList.add("item")
+  const existe = itens.find((elemento) => elemento.nome === nome.value);
 
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
 
-    novoItem.appendChild(numeroItem) 
-    novoItem.innerHTML += nome
+  if (existe) {
+    itemAtual.id = existe.id;
 
-    lista.appendChild(novoItem)
+    atualizaElemento(itemAtual);
+
+    itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
+  } else {
+    itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id +1 : 0;
+
+    criaElemento(itemAtual);
+
+    itens.push(itemAtual);
+  }
+
+  localStorage.setItem("itens", JSON.stringify(itens));
+
+  nome.value = "";
+  quantidade.value = "";
+});
+
+function criaElemento(item) {
+  const novoItem = document.createElement("li");
+  novoItem.classList.add("item");
+
+  const numeroItem = document.createElement("strong");
+  numeroItem.innerHTML = item.quantidade;
+  numeroItem.dataset.id = item.id;
+  novoItem.appendChild(numeroItem);
+
+  novoItem.innerHTML += item.nome;
+
+  novoItem.appendChild(butaoDeleta(item.id));
+
+  lista.appendChild(novoItem);
 }
 
+function atualizaElemento(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML =
+    item.quantidade;
+}
+
+function butaoDeleta(id) {
+  const elementoBotao = document.createElement("button");
+  elementoBotao.innerText = "X";
+
+  elementoBotao.addEventListener("click", function () {
+    deletaElemento(this.parentNode, id);
+  });
+
+  return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+  tag.remove();
+
+  itens.splice(
+    itens.findIndex((elemento) => elemento.id === id),
+    1
+  );
+
+  localStorage.setItem("itens", JSON.stringify(itens));
+
+}
